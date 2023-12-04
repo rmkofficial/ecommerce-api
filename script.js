23032fa5f9b383820164f6a2fa84e680f54188af
Ramazan Mert Kaya <kmert0@outlook.com>
2023-12-04 14:54:23 +0300

    Refactor HTML and JavaScript code, and add CSS styles

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     let products = document.querySelector('.products');
     async function fetchProducts(url) {
-        let data = await fetch(url);
-        let response = await data.json();
+        try {
+            let data = await fetch(url);
+            let response = await data.json();
 
-        for (let i = 0; i < response.length; i++) {
-            products.innerHTML += `
+            for (let i = 0; i < response.length; i++) {
+                let description = response[i].description;
+                products.innerHTML += `
         <div class="product">
-          <img src="" alt="" class="product-img" />
-          <h2 class="product-title">${response[i].title}</h2>
-          <h4 class="product-category"></h4>
-          <p class="product-description"></p>
+          <img src="${response[i].category.image}" alt="${response[i].category.name}" class="product-img" />
+          <div class="product-content">
+         <h2 class="product-title">${response[i].title}</h2>
+          <h4 class="product-category">${response[i].category.name}</h4>
+          <p class="product-description">${description.length > 20 ? description.substring(0, 20).concat('...more') : description}</p>
           <div class="product-price-container">
-            <h3 class="product-price"></h3>
-            <a href="#!" data-productId="" class="add-to-cart"></a>
+            <h3 class="product-price">$${response[i].price}</h3>
+            <a href="#!" data-productId="${response[i].id}" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+          </div>
           </div>
         </div>`;
+            }
+        }
+        catch (error) {
+            console.log(error);
         }
     };
     fetchProducts('https://api.escuelajs.co/api/v1/products');
