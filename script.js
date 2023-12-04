678277f0a6801ef9d39ae2a124735f496fb9457d
Ramazan Mert Kaya <kmert0@outlook.com>
2023-12-04 14:02:24 +0300

    Add product display to HTML

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let products = document.querySelector('.products');
     async function fetchProducts(url) {
         let data = await fetch(url);
         let response = await data.json();
-        console.log(response);
+
+        for (let i = 0; i < response.length; i++) {
+            products.innerHTML += `
+        <div class="product">
+          <img src="" alt="" class="product-img" />
+          <h2 class="product-title">${response[i].title}</h2>
+          <h4 class="product-category"></h4>
+          <p class="product-description"></p>
+          <div class="product-price-container">
+            <h3 class="product-price"></h3>
+            <a href="#!" data-productId="" class="add-to-cart"></a>
+          </div>
+        </div>`;
+        }
     };
     fetchProducts('https://api.escuelajs.co/api/v1/products');
 });
